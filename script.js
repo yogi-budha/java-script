d62cf632e151c24263f2bbdaabb82c399d177fc6
yogi-budha <bhimbudha959@gmail.com>
2023-08-26 23:43:18 +0545

    add the new file

---
+++ script.js
@@ -0,0 +1,100 @@
+// // object in object
+
+
+// const theList = ['Laurence','Svekis',true,35,null,undefined,{
+//     test:'one',
+//     score:55
+// },
+// ['one','two']];
+
+// theList.shift();
+
+// theList.unshift("First");
+
+
+
+// theList.pop();
+// theList.pop();
+
+// // MIDDLE,hello World LAST
+
+// theList.splice(2,7,"MIDDLE","hello World" , "LAST");
+
+// theList.pop();
+// theList.shift();
+
+// theList.
+
+
+// console.log(theList);
+
+// var inventory = [];
+//  var noodle= {
+//         name:"noodle",
+//         model:250,
+//         cost:20,
+//         quantity:99
+
+//     };
+
+//     var motor={
+//         name:"motor",
+//         model:19195,
+//         cost:2000,
+//         quantity:1
+
+//     };
+
+//     var fan={
+//         name:"fan",
+//         model:1222,
+//         cost:1000,
+//         quantity:10
+
+//     }
+
+//     inventory.push(noodle);
+//     inventory.unshift(motor);
+//     inventory.splice(2,0,fan)
+// console.log(inventory);
+
+// inventory[2].model=888;
+
+// console.log(inventory);
+
+// Q) can you use const and update values within an array?
+
+// -->
+
+// Array.length()-->gives the number of items contained in the
+
+
+// myArr=[1,3,5,6,9,15]
+
+// // myArr.indexOf(1)="4";
+
+// myArr.splice(1,1,4)
+
+// console.log(myArr);
+
+// const myArr2 = [];
+// myArr2[10] = 'test';
+// console.log(myArr2);
+// console.log(myArr2[2]);
+
+const myArr3 = [3, 6, 8, 9, 3, 55, 553, 434];
+myArr3.sort();
+
+myArr3.length = 0;
+console.log(myArr3);
+
+
+
+
+
+
+
+
+
+
+
